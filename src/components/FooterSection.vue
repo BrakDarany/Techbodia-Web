64a54e351ca1f5a0cd14b7fc859afafacbb1fc42
DaranyTB <darany.brak@techbodia.com>
2025-12-17 14:43:33 +0700

    Fixed margin on footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-slate-900 text-white py-6 md:py-10 px-6">
+  <footer class="bg-slate-900 text-white py-6 md:py-8 px-6">
     <div class="max-w-6xl mx-auto">
       <div class="flex flex-row justify-between items-center">
         <img class="h-12 w-12 lg:h-18 lg:w-18" src="../assets/Navbar/TB-logo.svg" alt="#">
