585fec0c9f7550a0754af6b8e58fb899c73d9c2d
DaranyTB <darany.brak@techbodia.com>
2025-12-31 14:06:32 +0700

    Refactor on about us

---
+++ src/components/AboutUs/OurCoreValuesSection.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="max-w-7xl mx-auto mt-24 lg:pr-7">
+    <div class="text-center mb-12">
+      <h2 class="text-2xl-primary-bold">
+        Our Core Values
+      </h2>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+    >
+      <OurCoreValueCard
+        v-for="(value, index) in ourCoreValues"
+        :key="index"
+        :title="value.title"
+        :description="value.description"
+        :icon-name="value.iconName"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ICoreValue } from '@/model/AboutUs';
+import OurCoreValueCard from './OurCoreValueCard.vue';
+
+const ourCoreValues: ICoreValue[] = [
+  {
+    title: 'Our Vision',
+    description: 'We constantly ask questions and seek new perspectives to drive innovation.',
+    iconName: 'be-curious',
+  },
+  {
+    title: 'Trust',
+    description: 'We build relationships based on transparency, honesty, and mutual respect.',
+    iconName: 'trust',
+  },
+  {
+    title: 'Continuous Learning',
+    description: 'We are committed to continuous learning and elevating our standards.',
+    iconName: 'never-stop-improve',
+  },
+  {
+    title: 'Evolution & Growth',
+    description: 'We regularly review our work and processes to evolve and overcome challenges.',
+    iconName: 'inspect-and-adapt',
+  },
+  {
+    title: 'Results Driven',
+    description: 'We stay focused on clear objectives to deliver measurable results.',
+    iconName: 'goal-driven',
+  },
+];
+</script>
